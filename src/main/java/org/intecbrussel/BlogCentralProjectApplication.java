44be93b8dd0a1dfd13b8140dc97b366d9aaf46bf
Tamara <toma_tomova@hotmail.com>
2025-12-10 11:59:17 +0100

    BlogCentralProject

---
+++ src/main/java/org/intecbrussel/BlogCentralProjectApplication.java
@@ -0,0 +1,13 @@
+package org.intecbrussel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogCentralProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogCentralProjectApplication.class, args);
+    }
+
+}
